5de5f897a934c731bc6e83c6e9edebde4af030bc
David <81187353+nob322@users.noreply.github.com>
2021-10-28 18:20:45 -0300

    Add files via upload

---
+++ validar.js
@@ -0,0 +1,20 @@
+function validar() {
+        var contieneValorCatcha = 776179;
+        var catcha = document.getElementById("catcha").value;
+    function cambiarClase(elIdSellama) {
+        const cambio = document.getElementById(elIdSellama).className = 'error';
+        return cambio;
+    }
+    if (catcha === "") {
+        cambiarClase("errorCatcha");
+        return false;
+    } else if (catcha != contieneValorCatcha) {
+        cambiarClase("errorCatchaIngresoErroneo");
+        return false;
+    } else if (catcha === contieneValorCatcha) {
+        cambiarClase("okCatchaOk");
+        return true;
+        //  setTimeout(esCorrecto,5000);
+
+    }
+}
